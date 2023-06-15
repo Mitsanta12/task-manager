2699778d7caa57861c1725110287e502ab9de0c3
Mitsanta_ANDRIAH <hei.mitsanta@gmail.com>
2023-06-15 11:34:07 +0300

    feat: add useTaskManager

--- src/store/useTaskManager.ts
+++ src/store/useTaskManager.ts
@@ -1,7 +1,29 @@
-const useTaskManager = () => {
+import create from 'zustand';
 
+interface Task {
+  id: number;
+  title: string;
 }
 
-export {
-  useTaskManager
-}
+const useTaskManager = create<Task[]>((set, get) => ({
+  tasks: [],
+  //for adding task with method push
+  addTask: (task: Task) => set((state) => {
+    state.tasks.push(task);
+  }),
+
+//for updating task
+  updateTask: (index: number, updatedTask: Task) => set((state) => {
+    state.tasks[index] = updatedTask;
+  }),
+
+  // for deleting a specific task by index
+  deleteTask: (index: number) => set((state) => {
+    state.tasks.splice(index, 1);
+  }),
+
+  // for searching another task in storage
+  searchTask: (title: string) => get((state) => state.tasks.find((task) => task.title === title)),
+}));
+
+export { useTaskManager };
