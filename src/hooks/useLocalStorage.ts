2d843a967488e7ffd68df9cb9cd156c38f1682ea
Mitsanta_ANDRIAH <hei.mitsanta@gmail.com>
2023-06-15 11:46:42 +0300

    feat: add useLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,19 @@
-const useLocalStorage = () => {
+import { useState, useEffect } from "react";
 
-}
+const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = useState(() => {
+    // Vérifie si une valeur existe déjà dans le Local Storage pour la clé donnée
+    const storedValue = localStorage.getItem(key);
+    // Renvoie la valeur stockée si elle existe, sinon renvoie la valeur initiale fournie
+    return storedValue ? JSON.parse(storedValue) : initialValue;
+  });
 
-export {
-  useLocalStorage
-}
+  useEffect(() => {
+    // Sauvegarde la valeur dans le Local Storage à chaque fois qu'elle est mise à jour
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export { useLocalStorage };
