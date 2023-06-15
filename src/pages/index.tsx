a1806294496b9d3273bbc8fd1c4360b134cd199a
Mitsanta_ANDRIAH <hei.mitsanta@gmail.com>
2023-06-15 10:39:34 +0300

    feat: add DiffTime

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,52 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 
+interface ServerTime {
+  toLocaleString: () => string;
+}
+
 /**
-  Calculates the time difference between the server time and client time.
-  @param {Date} serverTime - The server time.
-  @param {Date} clientTime - The client time.
-  @returns {string} The time difference in the format "{days} days, {hours} hours, {minutes} minutes, {seconds} seconds".
-*/
-const calculateTimeDifference = (server: Date, client: Date) => {};
+ * Calculates the time difference between the server time and client time.
+ * @param {Date} serverTime - The server time.
+ * @param {Date} clientTime - The client time.
+ * @returns {string} The time difference in the format "{days} days, {hours} hours, {minutes} minutes, {seconds} seconds".
+ */
+const calculateTimeDifference = (
+  serverTime: Date,
+  clientTime: Date
+) => {
+  // Calculate the time difference
+  const timeDiff = Math.abs(serverTime.getTime() - clientTime.getTime());
 
+  // Calculate the number of seconds in the time difference
+  const seconds = Math.floor(timeDiff / 1000) % 60;
 
-export default function Home() {
+  // Calculate the number of minutes in the time difference
+  const minutes = Math.floor(timeDiff / (1000 * 60)) % 60;
+
+  // Calculate the number of hours in the time difference
+  const hours = Math.floor(timeDiff / (1000 * 60 * 60)) % 24;
+
+  // Calculate the number of days in the time difference
+  const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+  return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+};
+
+interface HomeProps {
+  serverTime: ServerTime;
+}
+
+export default function Home({ serverTime }: HomeProps) {
   const router = useRouter();
+  const clientTime = new Date();
+
   const moveToTaskManager = () => {
     router.push("/tasks");
-  }
+  };
+
+  const timeDifference = calculateTimeDifference(serverTime, clientTime);
+
   return (
     <>
       <Head>
@@ -26,23 +58,27 @@ export default function Home() {
       <main>
         <h1>The easiest exam you will ever find</h1>
         <div>
-          {/* Display here the server time (DD-MM-AAAA HH:mm)*/}
           <p>
             Server time:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="serverTime">{serverTime.toLocaleString()}</span>
           </p>
-
-          {/* Display here the time difference between the server side and the client side */}
           <p>
-            Time diff:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            Time diff: <span className="serverTime">{timeDifference}</span>
           </p>
         </div>
-
         <div>
           <button onClick={moveToTaskManager}>Go to task manager</button>
         </div>
       </main>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const serverTime = new Date();
+  return {
+    props: {
+      serverTime,
+    },
+  };
 }
